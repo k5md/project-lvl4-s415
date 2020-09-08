8499fe3dc6c4062d7b94d28fae60672981f5826d
k5md <serg.dfc@yandex.ru>
2020-09-08 19:06:19 +0300

    change markdown to better match example project

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,20 +1,26 @@
 import React, { useCallback } from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 import { useChannels } from '../hooks';
 
 export default () => {
-  const channels = useChannels();
+  const { channelsList, currentChannelId } = useChannels();
 
   const renderChannel = useCallback(({ id, name }) => (
-    <ListGroup.Item key={id}>{name}</ListGroup.Item>
+    <Nav.Item key={id}>
+      <Nav.Link active={id === currentChannelId}>{name}</Nav.Link>
+    </Nav.Item>
   ), []);
 
   return (
     <>
-      <div className="mb-3">Channels</div>
-      <ListGroup>
-        {channels.map(renderChannel)}
-      </ListGroup>
+      <div className="d-flex mb-3 align-items-center justify-content-between">
+        <span>Channels</span>
+        <Button variant="link" className="shadow-none">+</Button>
+      </div>
+      <Nav variant="pills" className="flex-column">
+        {channelsList.map(renderChannel)}
+      </Nav>
     </>
   );
 };
